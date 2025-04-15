b70320e416594b65a313d8369296040784035fa7
MoulishwarGowd <Moulishwar.k@nybinfotech.com>
2025-04-15 18:01:44 +0530

    15/04/2025

--- javaSs/Functions/Today.js
+++ javaSs/Functions/Today.js
@@ -1,104 +1,127 @@
-// Function:-
+// // Function:-
 
-// it is a 2 types 
-// 1. function declaration
-// 2. function expression:-it s also a 3 types
-// a. function expression with named function
-// b. function expression with anonymous function
-// c. function expression with arrow function
-// function expression with named function:
-// sys:
-// function functionName() {
-//     return; // you can return a value here
-// }
-// function put() {
-//     return "Hello, World!";
-// }
-
-// console.log(put()); 
-
-// //function expression with anonymous function
-
+// // it is a 2 types 
+// // 1. function declaration
+// // 2. function expression:-it s also a 3 types
+// // a. function expression with named function
+// // b. function expression with anonymous function
+// // c. function expression with arrow function
+// // function expression with named function:
 // // sys:
-// const greet = function() {
-//     return "Hi there!";
-// };
+// // function functionName() {
+// //     return; // you can return a value here
+// // }
+// // function put() {
+// //     return "Hello, World!";
+// // }
 
-// greet();
+// // console.log(put()); 
 
-// //  function expression with arrow function
+// // //function expression with anonymous function
+
+// // // sys:
+// // const greet = function() {
+// //     return "Hi there!";
+// // };
+
+// // greet();
+
+// // //  function expression with arrow function
+// // // sys:
+// // const person = () => {
+// //     return "Hello from arrow function!";
+// // };
+
+// // person();
+
+
+
+// // Parameter and arguments (or) place holder
 // // sys:
-// const person = () => {
-//     return "Hello from arrow function!";
-// };
+// // function add(a, b) {
+// //     return a + b;
+// // }
+// // console.log(add(5, 7));
+// //  they are used to store the values of the function decleration
+// //  they are used to pass the values to the function declaration
 
-// person();
-
-
-
-// Parameter and arguments (or) place holder
-// sys:
-// function add(a, b) {
-//     return a + b;
-// }
-// console.log(add(5, 7));
-//  they are used to store the values of the function decleration
-//  they are used to pass the values to the function declaration
-
-// function add (a,b,c){
-    // console.log(a+b-c);
-//     console.log(a,b,c);
+// // function add (a,b,c){
+//     // console.log(a+b-c);
+// //     console.log(a,b,c);
     
 
     
-// }
-// add(12,300,300)
+// // }
+// // add(12,300,300)
 
 
-// // Defaltparameter
-// // sys:
-// function add(a,b=null,c=10){
-//     console.log(a+b+c);
-//     console.log(a,b,c);
-//     console.log(a*b*c);
+// // // Defaltparameter
+// // // sys:
+// // function add(a,b=null,c=10){
+// //     console.log(a+b+c);
+// //     console.log(a,b,c);
+// //     console.log(a*b*c);
     
-//     }
-//     add(12);
-//     //  default parameter is used to assign a default value to the parameter if no value is passed to
+// //     }
+// //     add(12);
+// //     //  default parameter is used to assign a default value to the parameter if no value is passed to
 
-//     // argumentsobjects---E.S-5
-//     // sys:
-//     function add(a,b,c){
-//         console.log(a+b+c);
-//         console.log(a,b,c);
-//         console.log(a*b*c);
-//         console.log(arguments);
+// //     // argumentsobjects---E.S-5
+// //     // sys:
+// //     function add(a,b,c){
+// //         console.log(a+b+c);
+// //         console.log(a,b,c);
+// //         console.log(a*b*c);
+// //         console.log(arguments);
         
-//         }
-//         add(12,300,300, 367645,7748,8484,84,);
+// //         }
+// //         add(12,300,300, 367645,7748,8484,84,);
         
 
 
-//         // rest parameter
-//         // sys:
-//         function add(a,b,c,...rest){
-//             console.log(a+b+c);
-//             console.log(a,b,c);
-//             console.log(rest);
-//         }
-//         add(12,300,300, 367645,7748,8484,84)
+// //         // rest parameter
+// //         // sys:
+// //         function add(a,b,c,...rest){
+// //             console.log(a+b+c);
+// //             console.log(a,b,c);
+// //             console.log(rest);
+// //         }
+// //         add(12,300,300, 367645,7748,8484,84)
 
 
-//         let A = 10;
-// const B = "Hello";
+// //         let A = 10;
+// // const B = "Hello";
 
-// console.log(A, B);
+// // console.log(A, B);
 
-// sum();
+// // sum();
 
-// function sum() {
+// // function sum() {
 
-// }
+// // }
+
+
+
+// // function sum(a, b) {
+// //     console.log(a + b);
+// //   }
+  
+// //   sum(1, 2);
+// // 1. function expression
+// // 2. function declaration
+// // 3. arrow function
+// // 4. function constructor
+// // var let const
+
+// // var a = 10;
+// // var b = 20;
+
+// // function sum() {
+// //   // block
+// //   console.log(a + b);
+// // }
+
+// // sum();
 
 
 
@@ -106,32 +129,9 @@
 //     console.log(a + b);
 //   }
   
-//   sum(1, 2);
-// 1. function expression
-// 2. function declaration
-// 3. arrow function
-// 4. function constructor
-// var let const
-
-// var a = 10;
-// var b = 20;
-
-// function sum() {
-//   // block
-//   console.log(a + b);
-// }
-
-// sum();
-
-
-
-function sum(a, b) {
-    console.log(a + b);
-  }
-  
-  sum(10, 20);
-  const a = 10;
-  const b = 20;
+//   sum(10, 20);
+//   const a = 10;
+//   const b = 20;
     
 
 
